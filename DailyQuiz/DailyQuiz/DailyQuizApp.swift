08d4c43ebc41eb21f773722fb57f0f8adaee8e33
shabanovdanila <168763335+shabanovdanila@users.noreply.github.com>
2025-08-01 13:31:07 +0300

    init

---
+++ DailyQuiz/DailyQuiz/DailyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuizApp.swift
+//  DailyQuiz
+//
+//  Created by Данила Шабанов on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
